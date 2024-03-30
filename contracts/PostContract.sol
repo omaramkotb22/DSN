f38155135d23c7b1d85cdf485f324150f57b85ba
Omar Kotb <omaramkotb22@gmail.com>
2024-03-30 17:22:51 +0800

    Added a new contract that references the code by post ID - 2

--- contracts/PostContract.sol
+++ contracts/PostContract.sol
@@ -3,21 +3,30 @@ pragma solidity ^0.8.24;
 
 contract PostContract {
     struct Post {
-        address author;
-        string title;
-        string content;
-        uint256 timestamp;
+        uint256 id; // The id of the post
+        address author; // The address of the author (creator of the post)
+        string title; // The title of the post
+        string content; // The content of the post
+        uint256 timestamp; 
     }
 
-    // Mapping from post index to a mapping of liker's addresses to boolean
+    //  Mapping from post index to a mapping of liker's addresses 
+    //  to boolean, either liked or not.
     mapping(uint256 => mapping(address => bool)) public likes;
 
     Post[] public posts;
 
-    event PostLiked(uint256 indexed postId, address indexed liker);
+    uint256 private lastPostId = 0; 
+
+    event PostWritten(uint256 indexed postId, address indexed author); // Event to notify when a post is written
+    event PostLiked(uint256 indexed postId, address indexed liker); // Event to notify when a post is liked
 
     function writePost(string memory _title, string memory _content) public {
-        posts.push(Post(msg.sender, _title, _content, block.timestamp));
+        lastPostId++; // Increment the last post id
+        Post memory newPost = Post(lastPostId, msg.sender, _title, _content, block.timestamp);
+        posts.push(newPost);
+        
+        emit PostWritten(lastPostId, msg.sender);
     }
 
     function getPosts() public view returns (Post[] memory) {
@@ -55,5 +64,11 @@ contract PostContract {
 
         emit LikeAction(_postId, msg.sender, _like);
 }
+
+    // Function to get the hash of a like action
+    function getHash(uint256 _postId, address _liker, bool _like) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_postId, _liker, _like));
+    }
+
 
 }
